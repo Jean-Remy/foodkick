ac36350080300d6eb6b4ee37950d404f8bfb64fa
Jeremy Nahum <js.nahum@gmail.com>
2015-08-31 18:54:39 +0200

    basic models creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831151411) do
+ActiveRecord::Schema.define(version: 20150831164856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "category"
+    t.integer  "price"
+    t.string   "name"
+    t.string   "description"
+    t.integer  "restaurant_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "courses", ["restaurant_id"], name: "index_courses_on_restaurant_id", using: :btree
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "restaurant_id"
+    t.string   "code"
+    t.string   "status"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "reservations", ["restaurant_id"], name: "index_reservations_on_restaurant_id", using: :btree
+  add_index "reservations", ["user_id"], name: "index_reservations_on_user_id", using: :btree
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "zip_code"
+    t.string   "city"
+    t.string   "borough"
+    t.string   "category"
+    t.string   "owner_first_name"
+    t.string   "owner_last_name"
+    t.string   "owner_phone"
+    t.string   "restaurant_phone"
+    t.string   "email"
+    t.text     "description"
+    t.integer  "price_min"
+    t.integer  "price_max"
+    t.float    "discount"
+    t.string   "discount_detail"
+    t.string   "menu_title"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "picture"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -29,9 +76,15 @@ ActiveRecord::Schema.define(version: 20150831151411) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "courses", "restaurants"
+  add_foreign_key "reservations", "restaurants"
+  add_foreign_key "reservations", "users"
 end
