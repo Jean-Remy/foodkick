aa012ca226e703169cf17799a9c0a08b7c304c9f
Jean Baptiste Beck <jean-baptiste.beck@edhec.com>
2015-09-01 15:51:53 +0200

    restaurants show & index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,9 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-
+    @restaurants = Restaurant.all
   end
 
   def show
@@ -27,5 +28,11 @@ class RestaurantsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
