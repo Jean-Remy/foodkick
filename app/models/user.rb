c4d33fc158bde0f6cf36b0676fdc4305f0af6992
Jean Baptiste Beck <jean-baptiste.beck@edhec.com>
2015-09-01 14:28:52 +0200

    adding confirmation to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
+         :recoverable, :confirmable, :rememberable, :trackable, :validatable, :omniauthable,
          omniauth_providers: [:facebook]
   def self.find_for_facebook_oauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -18,7 +18,6 @@ class User < ActiveRecord::Base
       user.picture = auth.info.image
       user.token = auth.credentials.token
       user.token_expiry = Time.at(auth.credentials.expires_at)
-      user.active = true
     end
   end
 
