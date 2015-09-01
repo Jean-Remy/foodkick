4789e67099930367a51416cc91717b0988656d84
Jean Baptiste Beck <jean-baptiste.beck@edhec.com>
2015-09-01 15:34:43 +0200

    seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901122720) do
+ActiveRecord::Schema.define(version: 20150901130945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,14 +89,14 @@ ActiveRecord::Schema.define(version: 20150901122720) do
     t.string   "picture"
     t.string   "first_name"
     t.string   "last_name"
-    t.boolean  "owner"
-    t.integer  "restaurant_id"
     t.string   "token"
     t.datetime "token_expiry"
     t.boolean  "active"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.boolean  "owner"
+    t.integer  "restaurant_id"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,64 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+restaurant_attributes = [
+  {
+    name: "Beijing 8",
+    address: "21 rue de la Boetie",
+    zip_code: "75008",
+    city: "Paris",
+    borough: "Champs-Elysées",
+    category: "Chinois",
+    price_min: 10,
+    price_max: 20,
+    description: "Concept  basé sur une restauration rapide et de qualité. Beijing 8 propose une gamme de dim sum et de thés dans une ambiance contemporaine et urbaine. Association de recettes ancestrales chinoises à des valeurs de développement durable.",
+    discount: 0.2,
+    discount_detail: "Remise valable pour l'achat d'un menu."
+  },
+  {
+    name: "Les Fables de la Fontaine",
+    address: "131 rue Saint Dominique",
+    zip_code: "75007",
+    city: "Paris",
+    borough: "Tour Eiffel",
+    category: "Cuisine Française",
+    price_min: 40,
+    price_max: 70,
+    description: "Les Fables de la Fontaine; restaurant 1* au Michelin; se sont offertes une beauté. Nouveau décor, nouvelle chef et baisse des prix à la carte. Ce restaurant crée il y plus de 15 ans par le grand chef Christian Constant, offre un choix d’excellence sur les poissons et fruits de mers, il est très vite devenu un des acteurs gourmands de la Rue Saint Dominique. La chef Julia Sedefdjian, reprend aujourd'hui les commandes de la cuisine.",
+    discount: 0.2,
+    discount_detail: "Remise valable sur l'ensemble de la carte"
+  },
+  {
+    name: "Biiim!",
+    address: "5 quai de Loire",
+    zip_code: "75019",
+    city: "Paris",
+    borough: "La Vilette",
+    category: "Burgers",
+    price_min: 15,
+    price_max: 30,
+    description: "Au bord du Canal de l'Ourcq, le nouveaux spot à burger, avant ou après une séance ciné, le resto biiim propose des produits frais et de qualité.",
+    discount: 0.2,
+    discount_detail: "Remise valable pour l'achat d'un burger."
+  }
+]
+
+user_attributes = [
+  {
+    email: "harry@potter.com",
+    first_name: "Harry",
+    last_name: "Potter",
+    password: "topsecret",
+    password_confirmation: "topsecret",
+    owner: true,
+    restaurant_id: 1
+  },
+  {
+    email: "ron@weasley.com",
+    first_name: "Ron",
+    last_name: "Weasley",
+    password: "topsecret",
+    password_confirmation: "topsecret",
+    owner: false
+  }
+]
+
+restaurant_attributes.each { |params| Restaurant.create(params)}
+user_attributes.each { |params| User.create(params)}
