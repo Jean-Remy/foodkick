8053400c450a6f8f77b2bdebaacecef364a6dd63
Jeremy Nahum <js.nahum@gmail.com>
2015-09-01 11:21:52 +0200

    set up controllers, models etc.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ActiveRecord::Base
   belongs_to :user
   belongs_to :restaurant
+  validates :name, presence: true
+  validates :address, presence: true
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ActiveRecord::Base
+  has_many :courses
+  has_many :reservations
+  has_one :schedule
 end
