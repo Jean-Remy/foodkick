1fc03e473ac2bfbaebc8d119666f06323ecc96c6
Jean Baptiste Beck <jean-baptiste.beck@edhec.com>
2015-09-04 14:53:43 +0200

    Restaurant index

---
+++ app/assets/javascripts/card.js
@@ -0,0 +1,13 @@
+// $( document ).ready(function() {
+//   $(".card")
+//     .mouseenter(function() {
+//       $(this).children($(".card-description")).toggle();
+//       $(".card-text").toggle();
+//       $(".card-search").toggle();
+//     })
+//     .mouseleave(function() {
+//       $(".card-description").toggle();
+//       $(".card-text").toggle();
+//       $(".card-search").toggle();
+//     })
+// });
