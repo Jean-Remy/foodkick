d62254391a19b4a3ea92af6dd1f2c4ce572d353e
Jeremy Nahum <js.nahum@gmail.com>
2015-09-04 14:21:20 +0200

    seeds restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,8 @@ restaurant_attributes = [
     description: "Concept  basé sur une restauration rapide et de qualité. Beijing 8 propose une gamme de dim sum et de thés dans une ambiance contemporaine et urbaine. Association de recettes ancestrales chinoises à des valeurs de développement durable.",
     discount: 0.2,
     discount_detail: "Remise valable pour l'achat d'un menu.",
-    seed_picture: "https://images.unsplash.com/reserve/oHPXcFZQ9SyeGD5F7T1g_ramenya.jpg?q=80&fm=jpg&s=92509ecd9a51138caebd5a0a77dba3c0"
+    seed_picture: "https://images.unsplash.com/reserve/oHPXcFZQ9SyeGD5F7T1g_ramenya.jpg?q=80&fm=jpg&s=92509ecd9a51138caebd5a0a77dba3c0",
+    opening: 2015-09-01
   },
   {
     name: "Les Fables de la Fontaine",
@@ -25,7 +26,8 @@ restaurant_attributes = [
     description: "Les Fables de la Fontaine; restaurant 1* au Michelin; se sont offertes une beauté. Nouveau décor, nouvelle chef et baisse des prix à la carte. Ce restaurant crée il y plus de 15 ans par le grand chef Christian Constant, offre un choix d’excellence sur les poissons et fruits de mers, il est très vite devenu un des acteurs gourmands de la Rue Saint Dominique. La chef Julia Sedefdjian, reprend aujourd'hui les commandes de la cuisine.",
     discount: 0.2,
     discount_detail: "Remise valable sur l'ensemble de la carte",
-    seed_picture: "https://images.unsplash.com/photo-1426259759666-68da5c54402d?q=80&fm=jpg&s=5fa8cad7a9333b2e1a46f4174fa88dc5"
+    seed_picture: "https://images.unsplash.com/photo-1426259759666-68da5c54402d?q=80&fm=jpg&s=5fa8cad7a9333b2e1a46f4174fa88dc5",
+    opening: 2015-08-01
   },
   {
     name: "Biiim!",
@@ -39,7 +41,188 @@ restaurant_attributes = [
     description: "Au bord du Canal de l'Ourcq, le nouveaux spot à burger, avant ou après une séance ciné, le resto biiim propose des produits frais et de qualité.",
     discount: 0.2,
     discount_detail: "Remise valable pour l'achat d'un burger.",
-    seed_picture: "https://images.unsplash.com/photo-1416453072034-c8dbfa2856b5?q=80&fm=jpg&s=cbea81b9705e4553fbb2d7681aa5731e"
+    seed_picture: "https://images.unsplash.com/photo-1416453072034-c8dbfa2856b5?q=80&fm=jpg&s=cbea81b9705e4553fbb2d7681aa5731e",
+    opening: 2015-08-01
+  },
+    {
+    name: "NA/NA",
+    address: "10, rue Bréguet",
+    zip_code: "75011 ",
+    city: "Paris",
+    borough: "Champs-Elysées",
+    category: "Original",
+    price_min: 10,
+    price_max: 20,
+    description: "Du matin au soir, les trois supers NANA, vous proposent des produits frais, de qualité et de saison. Un petit coin de calme et de fraicheur, au centre du quartier animé de Bastille",
+    discount: 0.2,
+    discount_detail: "Remise valable pour l'achat d'un menu.",
+    seed_picture: "https://unsplash.com/photos/WIJPPnoVrDs/download",
+    opening: 2015-08-01
+  },
+  {
+    name: "Fagio",
+    address: "72, rue de Rochechouart",
+    zip_code: "75009",
+    city: "Paris",
+    borough: "Tour Eiffel",
+    category: "Cuisine Française",
+    price_min: 40,
+    price_max: 70,
+    description: "Les Fables de la Fontaine; restaurant 1* au Michelin; se sont offertes une beauté. Nouveau décor, nouvelle chef et baisse des prix à la carte. Ce restaurant crée il y plus de 15 ans par le grand chef Christian Constant, offre un choix d’excellence sur les poissons et fruits de mers, il est très vite devenu un des acteurs gourmands de la Rue Saint Dominique. La chef Julia Sedefdjian, reprend aujourd'hui les commandes de la cuisine.",
+    discount: 0.2,
+    discount_detail: "Remise valable sur l'ensemble de la carte",
+    seed_picture: "https://unsplash.com/photos/LY9GKi4xllY/download"
+    opening: 2015-08-01
+  },
+  {
+    name: "750g la table",
+    address: "397 rue de Vaugirard",
+    zip_code: "75015",
+    city: "Paris",
+    borough: "La Vilette",
+    category: "Terrasse",
+    price_min: 15,
+    price_max: 30,
+    description: "Au bord du Canal de l'Ourcq, le nouveaux spot à burger, avant ou après une séance ciné, le resto biiim propose des produits frais et de qualité.",
+    discount: 0.2,
+    discount_detail: "Remise valable pour l'achat d'un burger.",
+    seed_picture: "https://unsplash.com/photos/4vr9a_sdJ78/download"
+    opening: 2015-08-01
+  },
+   {
+    name: "Clasico Argentino",
+    address: "Passage des Panoramas",
+    zip_code: "75002",
+    city: "Paris",
+    borough: "Champs-Elysées",
+    category: "Argentin",
+    price_min: 10,
+    price_max: 20,
+    description: "Concept  basé sur une restauration rapide et de qualité. Beijing 8 propose une gamme de dim sum et de thés dans une ambiance contemporaine et urbaine. Association de recettes ancestrales chinoises à des valeurs de développement durable.",
+    discount: 0.2,
+    discount_detail: "Remise valable pour l'achat d'un menu.",
+    seed_picture: "https://unsplash.com/photos/6gcc9Z4pgWY/download"
+    opening: 2015-08-01
+  },
+  {
+    name: "Gabilou",
+    address: "15 rue d'Armaillé",
+    zip_code: "75017",
+    city: "Paris",
+    borough: "Tour Eiffel",
+    category: "Contemporain",
+    price_min: 40,
+    price_max: 70,
+    description: "Les Fables de la Fontaine; restaurant 1* au Michelin; se sont offertes une beauté. Nouveau décor, nouvelle chef et baisse des prix à la carte. Ce restaurant crée il y plus de 15 ans par le grand chef Christian Constant, offre un choix d’excellence sur les poissons et fruits de mers, il est très vite devenu un des acteurs gourmands de la Rue Saint Dominique. La chef Julia Sedefdjian, reprend aujourd'hui les commandes de la cuisine.",
+    discount: 0.2,
+    discount_detail: "Remise valable sur l'ensemble de la carte",
+    seed_picture: "https://unsplash.com/photos/MtqG1lWcUw0/download"
+    opening: 2015-08-01
+  },
+  {
+    name: "Desiroad!",
+    address: "14 rue Dauphine",
+    zip_code: "75006",
+    city: "Paris",
+    borough: "La Vilette",
+    category: "Original",
+    price_min: 15,
+    price_max: 30,
+    description: "Au bord du Canal de l'Ourcq, le nouveaux spot à burger, avant ou après une séance ciné, le resto biiim propose des produits frais et de qualité.",
+    discount: 0.2,
+    discount_detail: "Remise valable pour l'achat d'un burger.",
+    seed_picture: "https://unsplash.com/photos/4qJdtfJ2MmQ/download"
+    opening: 2015-08-01
+  },
+    {
+    name: "Le Refectoire",
+    address: "31 Rue du château d'eau",
+    zip_code: "75010",
+    city: "Paris",
+    borough: "Champs-Elysées",
+    category: "Burger",
+    price_min: 10,
+    price_max: 20,
+    description: "Concept  basé sur une restauration rapide et de qualité. Beijing 8 propose une gamme de dim sum et de thés dans une ambiance contemporaine et urbaine. Association de recettes ancestrales chinoises à des valeurs de développement durable.",
+    discount: 0.2,
+    discount_detail: "Remise valable pour l'achat d'un menu.",
+    seed_picture: "https://unsplash.com/photos/jivmv9hE6bM/download"
+    opening: 2015-08-01
+  },
+  {
+    name: "Gallika",
+    address: "7 rue Godot de Mauroy",
+    zip_code: "75008",
+    city: "Paris",
+    borough: "Tour Eiffel",
+    category: "Cuisine Française",
+    price_min: 40,
+    price_max: 70,
+    description: "Les Fables de la Fontaine; restaurant 1* au Michelin; se sont offertes une beauté. Nouveau décor, nouvelle chef et baisse des prix à la carte. Ce restaurant crée il y plus de 15 ans par le grand chef Christian Constant, offre un choix d’excellence sur les poissons et fruits de mers, il est très vite devenu un des acteurs gourmands de la Rue Saint Dominique. La chef Julia Sedefdjian, reprend aujourd'hui les commandes de la cuisine.",
+    discount: 0.2,
+    discount_detail: "Remise valable sur l'ensemble de la carte",
+    seed_picture: "https://unsplash.com/photos/y3aP9oo9Pjc/download"
+    opening: 2015-07-01
+  },
+  {
+    name: "Nomos",
+    address: "15 rue André del Sarte",
+    zip_code: "75018",
+    city: "Paris",
+    borough: "La Vilette",
+    category: "Bobo",
+    price_min: 15,
+    price_max: 30,
+    description: "Au bord du Canal de l'Ourcq, le nouveaux spot à burger, avant ou après une séance ciné, le resto biiim propose des produits frais et de qualité.",
+    discount: 0.2,
+    discount_detail: "Remise valable pour l'achat d'un burger.",
+    seed_picture: "https://unsplash.com/photos/dfItZYL3qI0/download"
+    opening: 2015-07-01
+  },
+    {
+    name: "Le Bat",
+    address: "16 bis, boulevard Montmartre",
+    zip_code: "75009",
+    city: "Paris",
+    borough: "Champs-Elysées",
+    category: "Terrasse",
+    price_min: 10,
+    price_max: 20,
+    description: "Concept  basé sur une restauration rapide et de qualité. Beijing 8 propose une gamme de dim sum et de thés dans une ambiance contemporaine et urbaine. Association de recettes ancestrales chinoises à des valeurs de développement durable.",
+    discount: 0.2,
+    discount_detail: "Remise valable pour l'achat d'un menu.",
+    seed_picture: "https://unsplash.com/photos/N_Y88TWmGwA/download"
+    opening: 2015-07-01
+  },
+  {
+    name: "Les Fables de la Fontaine",
+    address: "2 rue Auguste Vacquerie",
+    zip_code: "75016",
+    city: "Paris",
+    borough: "Tour Eiffel",
+    category: "Japonais",
+    price_min: 40,
+    price_max: 70,
+    description: "Les Fables de la Fontaine; restaurant 1* au Michelin; se sont offertes une beauté. Nouveau décor, nouvelle chef et baisse des prix à la carte. Ce restaurant crée il y plus de 15 ans par le grand chef Christian Constant, offre un choix d’excellence sur les poissons et fruits de mers, il est très vite devenu un des acteurs gourmands de la Rue Saint Dominique. La chef Julia Sedefdjian, reprend aujourd'hui les commandes de la cuisine.",
+    discount: 0.2,
+    discount_detail: "Remise valable sur l'ensemble de la carte",
+    seed_picture: "https://unsplash.com/photos/-nBw28ei-OE/download"
+    opening: 2015-07-01
+  },
+  {
+    name: "Season",
+    address: "1 rue Charles Francois Dupuis",
+    zip_code: "75003",
+    city: "Paris",
+    borough: "La Vilette",
+    category: "Cuisine française",
+    price_min: 15,
+    price_max: 30,
+    description: "Au bord du Canal de l'Ourcq, le nouveaux spot à burger, avant ou après une séance ciné, le resto biiim propose des produits frais et de qualité.",
+    discount: 0.2,
+    discount_detail: "Remise valable pour l'achat d'un burger.",
+    seed_picture: "https://unsplash.com/photos/o58cEDAnPB8/download"
+    opening: 2015-07-01
   }
 ]
 
