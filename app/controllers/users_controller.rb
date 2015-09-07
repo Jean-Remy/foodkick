d197e85178afe865c6efd632f57878315f1b3908
Louis Baudoin <louis.baudoin0102@gmail.com>
2015-09-07 14:37:41 +0200

    basics du show profile du user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,10 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     authorize @user
+
+    @coming_reservations = @user.reservations.where(status: false)
+    # authorize @coming_reservations
+    @past_reservations = @user.reservations.where(status: true)
+    # authorize @past_reservations
   end
 end
